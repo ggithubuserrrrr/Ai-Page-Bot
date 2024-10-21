4d56b9015584569317f639acde2908c9c36c3dbc
Rui Reogo <149220283+ruingl@users.noreply.github.com>
2024-10-21 13:27:25 +0800

    update handleMessage.js

--- handles/handleMessage.js
+++ handles/handleMessage.js
@@ -1,15 +1,48 @@
 const fs = require('fs');
 const path = require('path');
+const request = require('request');
 const { sendMessage } = require('./sendMessage');
 
 const commands = new Map();
-
-// Load all command modules dynamically
 const commandFiles = fs.readdirSync(path.join(__dirname, '../commands')).filter(file => file.endsWith('.js'));
 for (const file of commandFiles) {
   const command = require(`../commands/${file}`);
   commands.set(command.name, command);
 }
+
+// rui: add commands list
+async function setupCommands(pageAccessToken) {
+  const commandsData = {
+    commands: [
+      {
+        locale: "default",
+        commands: Array.from(commands.values()).map(command => ({
+          name: command.name,
+          description: `${command.description} (by ${command.author})`
+        }))
+      }
+    ]
+  };
+
+  return new Promise((resolve, reject) => {
+    request({
+      uri: `https://graph.facebook.com/v21.0/me/messenger_profile`,
+      qs: { access_token: pageAccessToken },
+      method: 'POST',
+      json: commandsData
+    }, (error, response, body) => {
+      if (!error && response.statusCode === 200) {
+        console.log('Commands set successfully:', body);
+        resolve(body);
+      } else {
+        console.error('Error setting commands:', error || body.error);
+        reject(error || body.error);
+      }
+    });
+  });
+};
+
+setupCommands();
 
 async function handleMessage(event, pageAccessToken) {
   const senderId = event.sender.id;
